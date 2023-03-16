d38e54847687c5a9561b92fd3b08617b065e026c
Valeria <valeriastetsiuk3546@gmail.com>
2023-03-16 14:05:59 +0200

    add style HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,45 @@
 import { Section } from 'utils/globalStyles';
 import { PageTitle } from '../../components/Common/PageTitle/PageTitle';
-
+import {
+  BoxText,
+  Img,
+  Box,
+  Container,
+  BoxImg,
+  Span,
+  PreTitle,
+} from './HomePage.styled';
+import PhotoOfOlena from '../../images/Olena.jpg';
+import Light from '../../images/light.jpg';
 export const HomePage = () => {
   return (
     <main>
       <Section>
-        <PageTitle text="Hello" />
+        <PageTitle text="Вітаю, друзі!" />
+        <Box>
+          <BoxText>
+            Ваш час обмежений, тому не витрачайте його, проживаючи чиєсь чуже
+            життя. Не попадайтеся в пастку догми – не живіть чужими думками. Не
+            давайте шуму чужих думок заглушати власний внутрішній голос. І
+            найголовніше – майте мужність слідувати своєму серцю і розуму. Вони
+            якимось чином вже знають, ким ти повинен стати. Все інше – вторинне.
+          </BoxText>
+          <Img src={PhotoOfOlena} alt="Photo of Olena" />
+        </Box>
+        <Container>
+          <BoxImg src={Light} alt="light" />
+          <p>
+            <Span> Я Олена Бра </Span>- дипломований практикуючий
+            психолог-консультант, коуч. Працюю онлайн у форматі аудіо-або
+            відеодзвінок.
+            <PreTitle>
+              Вважаю, що наявність дипломів, досвід роботи з клієнтами є
+              важливими, при цьому на перше місце ставлю особистість людини,
+              тому перша міні сесія є безкоштовною для того, щоб ви відчули
+              наскільки вам комфортно працювати зі мною.
+            </PreTitle>
+          </p>
+        </Container>
       </Section>
     </main>
   );
---
+++ src/pages/HomePage/HomePage.styled.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+export const BoxText = styled.div`
+  background: rgba(240, 251, 251, 0.65);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+  width: 654px;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 1.5;
+  letter-spacing: 0.05em;
+  text-align: center;
+  padding: 25px;
+  color: #262323;
+`;
+export const Img = styled.img`
+  border-radius: 40px;
+  width: 387px;
+`;
+export const BoxImg = styled.img`
+  border-radius: 40px;
+  width: 338px;
+`;
+export const Box = styled.div`
+  display: flex;
+  gap: 65px;
+  align-items: center;
+`;
+export const Container = styled.div`
+  display: flex;
+  gap: 65px;
+  margin-top: 70px;
+  align-items: center;
+  background: rgba(240, 251, 251, 0.65);
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 1.6;
+  letter-spacing: 0.05em;
+  padding: 30px;
+`;
+export const Span = styled.span`
+  font-weight: 700;
+`;
+export const PreTitle = styled.p`
+  margin-top: 30px;
+`;
