97dd40111a4f998b27f390354fa74670860a461a
Valeria <valeriastetsiuk3546@gmail.com>
2023-03-17 21:01:13 +0200

    add style AboutPage + fix style ContactsPage

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,11 +1,43 @@
 import { Section } from 'utils/globalStyles';
 import { PageTitle } from '../../components/Common/PageTitle/PageTitle';
-
+import PhotoOfOlena from '../../images/Olena1.jpg';
+import {
+  Box,
+  BoxText,
+  Img,
+  Caption,
+  List,
+  Span,
+} from '../AboutPage/AboutPage.styled';
 export const AboutPage = () => {
   return (
     <main>
       <Section>
         <PageTitle text="Про мене" />
+        <Box>
+          <Img src={PhotoOfOlena} alt="Photo of Olena" />
+          <BoxText>
+            <p>
+              <Span>Олена Бра</Span> - дипломований практикуючий
+              психолог-консультант, коуч.
+            </p>
+            <p>
+              В особистій терапії 3 роки, продовжую працювати над собою до цього
+              дня.
+            </p>
+            <p>
+              Постійно беру участь у груповій терапії. Відвідую майстер класи та
+              продовжую навчатися далі.
+            </p>
+            <Caption>Поглиблено вивчала такі теми як:</Caption>
+            <List>
+              <li>самоцінність</li>
+              <li>відповідальність</li>
+              <li>особисті межі</li>
+              <li>відносини між чоловіком та жінкою</li>
+            </List>
+          </BoxText>
+        </Box>
       </Section>
     </main>
   );
---
+++ src/pages/AboutPage/AboutPage.styled.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+export const BoxText = styled.div`
+  background: rgba(240, 251, 251, 0.65);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+  width: 654px;
+
+  font-size: 20px;
+  line-height: 1.5;
+  letter-spacing: 0.05em;
+  padding: 25px;
+  color: #262323;
+`;
+export const Img = styled.img`
+  border-radius: 40px;
+  width: 387px;
+`;
+export const Box = styled.div`
+  display: flex;
+  gap: 65px;
+  align-items: center;
+`;
+export const Caption = styled.h2`
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 1.6;
+  text-align: center;
+
+  margin: 10px;
+`;
+export const List = styled.ul`
+  list-style: disc;
+  padding: 0 25px 0;
+`;
+export const Span = styled.span`
+  font-weight: 600;
+`;
