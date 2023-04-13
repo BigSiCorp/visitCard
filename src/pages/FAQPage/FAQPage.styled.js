fb9f2fec86d9c0e366c6d6d67f6216212e4c5383
Valeria <valeriastetsiuk3546@gmail.com>
2023-04-13 18:09:14 +0300

    add some changes (onclick FAQPage)

--- src/pages/FAQPage/FAQPage.styled.js
+++ src/pages/FAQPage/FAQPage.styled.js
@@ -22,7 +22,7 @@ export const FAQIteam = styled.li`
   width: 499px;
   display: flex;
   flex-direction: column;
-  height: 329px;
+  // height: 329px;
   padding: 30px;
   background: rgba(255, 254, 254, 0.68);
   border-radius: 20px;
@@ -31,6 +31,7 @@ export const FAQTitle = styled.p`
   font-weight: 700;
   text-alight: center;
 `;
+
 export const FAQBox = styled.div`
   display: flex;
   align-items: center;
