7544f3c37a45c74deffd7f7cc0be496d7bbcacb0
BigSiCorp <si.kocorp@gmail.com>
2023-04-18 20:14:24 +0200

    edit newsPage

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -1,6 +1,7 @@
 import { Section } from 'utils/globalStyles';
 import { PageTitle } from '../../components/Common/PageTitle/PageTitle';
 import Photo from '../../images/1.png';
+import { news } from '../../data/news';
 import { Box, Img } from './NewsPage.styled';
 export const BlogPage = () => {
   return (
@@ -8,7 +9,17 @@ export const BlogPage = () => {
       <Section>
         <PageTitle text="Новости" />
         <Box>
-          <Img src={Photo} alt="Photo" />
+          <ul>
+            {news.length > 0 &&
+              news.map(({ id, title, text }) => (
+                <li key={id}>
+                  <h3>{title}</h3>
+                  <p>{text}</p>
+                </li>
+              ))}
+          </ul>
+
+          {news.length === 0 && <Img src={Photo} alt="Photo" />}
         </Box>
       </Section>
     </main>
